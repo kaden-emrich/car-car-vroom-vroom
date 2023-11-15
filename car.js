f9bd0422881a92aa86a97f1a309107d8cebd49d7
Kaden Emrich <kadenemrich@gmail.com>
2023-11-15 14:24:20 -0500

    background

--- car.js
+++ car.js
@@ -9,25 +9,59 @@ class Car {
         this.throttle = 0;
         this.steering = 0;
 
-        this.acceleration = 0.2;
-        this.friction = 0.05;
+        this.acceleration = 0.1;
+        this.friction = 0.03;
+        this.decelerationBonus = 0.1;
 
         this.turnSpeed = 0.03;
+        this.turnBuffer = 3;
         this.speed = 0;
-        this.maxSpeed = 10;
+        this.maxSpeed = 20;
 
         this.staticTurn = false;
 
         this.color = 'black';
 
         this.viewType = viewType;
+
+        this.forward = false;
+        this.reverse = false;
+        this.left = false;
+        this.right = false;
     }
 
     update() {
-        this.speed += this.acceleration * this.throttle;
-        
-        if(this.speed > this.maxSpeed) {
-            this.speed = this.maxSpeed;
+        if(this.forward && this.reverse) {
+            this.throttle = 0;
+        } 
+        else if(this.forward) {
+            this.throttle = 1;
+        }
+        else if(this.reverse) {
+            this.throttle = -1;
+        }
+        else {
+            this.throttle = 0;
+        }
+
+        if(this.left && this.right) {
+            this.steering = 0;
+        }
+        else if(this.left) {
+            this.steering = 1;
+        }
+        else if(this.right) {
+            this.steering = -1;
+        }
+        else {
+            this.steering = 0;
+        }
+
+        if(this.speed * this.throttle < 0) {
+            this.speed += (this.acceleration + this.decelerationBonus) * this.throttle;
+        }
+        else {
+            this.speed += this.acceleration * this.throttle;
         }
 
         if(this.speed > 0) {
@@ -39,10 +73,22 @@ class Car {
         if(Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
+        
+        if(this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        if(this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
 
         if(Math.abs(this.speed) > 0) {
-            let flip = this.speed > 0 ? 1 : -1;
-            this.direction += this.turnSpeed * this.steering * flip;
+            if(Math.abs(this.speed) < this.turnBuffer) {
+                this.direction += ((this.speed / this.turnBuffer) * this.turnSpeed) * this.steering;
+            }
+            else {
+                let flip = this.speed > 0 ? 1 : -1;
+                this.direction += this.turnSpeed * this.steering * flip;
+            }
         }
         else if(this.staticTurn){
             this.direction += this.turnSpeed * this.steering;
--- main.js
+++ main.js
@@ -1,26 +1,58 @@
 const gameCanvas = document.getElementById('game-canvas');
 const gameCtx = gameCanvas.getContext('2d');
 
+var camera = {
+    rotate: true,
+    follow: true
+}
+
 var car1 = new Car(30, 50, 250, 250, 'default');
 car1.staticTurn = false;
-car1.color = 'blue';
+car1.color = 'red';
 var car2 = new Car(30, 50, 300, 250);
 
 var carController = new Controller([
-    new KeyHandler(['w', 'W', 'ArrowUp'], () => {car1.throttle = 1;}, () => {car1.throttle = 0;}),
-    new KeyHandler(['s', 'S', 'ArrowDown'], () => {car1.throttle = -1;}, () => {car1.throttle = 0;}),
-    new KeyHandler(['a', 'A', 'ArrowLeft'], () => {car1.steering = 1;}, () => {car1.steering = 0;}),
-    new KeyHandler(['d', 'D', 'ArrowRight'], () => {car1.steering = -1;}, () => {car1.steering = 0;})
+    new KeyHandler(['w', 'W', 'ArrowUp'], () => {car1.forward = true;}, () => {car1.forward = false;}),
+    new KeyHandler(['s', 'S', 'ArrowDown'], () => {car1.reverse = true;}, () => {car1.reverse = false;}),
+    new KeyHandler(['a', 'A', 'ArrowLeft'], () => {car1.left = true;}, () => {car1.left = false;}),
+    new KeyHandler(['d', 'D', 'ArrowRight'], () => {car1.right = true;}, () => {car1.right = false;})
 ]);
 
-function drawTestBackground(ctx, gridSize = 20) {
-    ctx.strokeStyle = 'lightBlue';
+function drawSpeedometer(car) {
+    gameCtx.fillStyle = 'red';
+    gameCtx.fillRect(0, gameCanvas.height, lerp(0, gameCanvas.width, Math.abs(car.speed / car.maxSpeed)), -10);
+}
 
-    for(let i = 0; i < 25; i++) {
-        for(let j = 0; j < 25; j++) {
-            ctx.strokeRect(i*gridSize, j*gridSize, gridSize, gridSize);
+function drawTestBackground(gridSize = 50) {
+    let backgroundSize = 5000;
+
+    gameCtx.strokeStyle = 'lightBlue';
+    gameCtx.lineWidth = 3;
+
+    for(let i = 0; i < (backgroundSize / gridSize); i++) {
+        for(let j = 0; j < (backgroundSize / gridSize); j++) {
+            gameCtx.strokeRect(i*gridSize - ((backgroundSize + gameCanvas.width) / 2), j*gridSize - ((backgroundSize + gameCanvas.height) / 2), gridSize, gridSize);
         }
     }
+
+    gameCtx.lineWidth = 5;
+
+    gameCtx.strokeStyle = 'pink';
+    gameCtx.beginPath();
+    gameCtx.moveTo(((backgroundSize + gameCanvas.width) / 2), gameCanvas.width / 2);
+    gameCtx.lineTo((-(backgroundSize + gameCanvas.width) / 2), gameCanvas.width / 2);
+    gameCtx.stroke();
+    
+    gameCtx.closePath();
+
+    gameCtx.strokeStyle = 'lightGreen';
+    gameCtx.beginPath();
+    gameCtx.moveTo(gameCanvas.height / 2, ((backgroundSize + gameCanvas.height) / 2));
+    gameCtx.lineTo(gameCanvas.height / 2, (-(backgroundSize + gameCanvas.height) / 2));
+    gameCtx.stroke();
+
+    gameCtx.strokeStyle = 'transparent';
+    gameCtx.stroke();
 }
 
 function nextFrame() {
@@ -30,16 +62,25 @@ function nextFrame() {
     car1.update();
     car2.update();
 
-    gameCtx.save();
-    gameCtx.translate(gameCanvas.width/2, gameCanvas.height/2);
-    gameCtx.rotate(car1.direction);
-    gameCtx.translate(-car1.x, -car1.y);
+    if(camera.follow) {
+        gameCtx.save();
+        gameCtx.translate(gameCanvas.width/2, gameCanvas.height/2);
 
-    drawTestBackground(gameCtx);
+        if(camera.rotate) {
+            gameCtx.rotate(car1.direction);
+        }
+
+        gameCtx.translate(-car1.x, -car1.y);
+    }
+
+    drawTestBackground();
+    
     car1.draw(gameCtx);
     car2.draw(gameCtx);
 
     gameCtx.restore();
+
+    drawSpeedometer(car1);
 }
 
 nextFrame();
